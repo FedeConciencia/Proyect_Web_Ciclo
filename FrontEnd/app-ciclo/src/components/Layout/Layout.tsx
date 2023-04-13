3b0e7eb3854af53a99f78a836a6d777a7b08784e
Roman Maluf <romanmaluf@vaypol.com.ar>
2023-04-12 23:35:48 -0300

    add favicon

--- FrontEnd/app-ciclo/src/components/Layout/Layout.tsx
+++ FrontEnd/app-ciclo/src/components/Layout/Layout.tsx
@@ -35,6 +35,11 @@ const Layout = ({
         <meta name="twitter:description" content={description} />
         <link rel="canonical" href={`${canonicalURL}​​​​​​`} />
         <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
       </Head>
       <Header/>
       {children}​​​​​​ {/* Aca se re renderiza el componente hijo */}
