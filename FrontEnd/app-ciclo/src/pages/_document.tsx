245cf5974b2159f70b87810650e01e6115e9030d
Roman Maluf <romanmaluf@vaypol.com.ar>
2023-04-23 21:48:46 -0300

    poppins font-family

--- FrontEnd/app-ciclo/src/pages/_document.tsx
+++ FrontEnd/app-ciclo/src/pages/_document.tsx
@@ -9,6 +9,10 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;700&display=swap"
           rel="stylesheet"
         />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;700&display=swap"
+          rel="stylesheet"
+        />
       </Head>
       <body>
         <Main />
