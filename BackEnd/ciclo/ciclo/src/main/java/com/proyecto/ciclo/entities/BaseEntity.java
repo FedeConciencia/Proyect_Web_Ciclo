5f80a3d6f5714e24c6df59617a713005550458c0
Roman Maluf <romanmaluf@vaypol.com.ar>
2023-06-14 23:04:39 -0300

    cambiar nombre de base.java a baseEntity.java && agregar valores por defecto para el historico de fechas, y filtros por defecto para evitar los eliminados

---
+++ BackEnd/ciclo/ciclo/src/main/java/com/proyecto/ciclo/entities/BaseEntity.java
@@ -0,0 +1,110 @@
+package com.proyecto.ciclo.entities;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import org.hibernate.annotations.Filter;
+import org.hibernate.annotations.FilterDef;
+import org.hibernate.annotations.ParamDef;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
+import javax.validation.constraints.NotNull;
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.time.OffsetDateTime;
+import java.time.ZoneOffset;
+
+@MappedSuperclass
+@FilterDef(name = "deletedEntityFilter", parameters = @ParamDef(name = "deleted", type = "boolean"))
+@Filter(name = "deletedEntityFilter", condition = "deleted_at IS NULL OR deleted = :deleted")
+public class BaseEntity implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "date_create")
+    @JsonFormat(shape=JsonFormat.Shape.STRING, pattern="yyyy-MM-dd")
+    private LocalDate date_create;
+
+
+    @NotNull
+    @Column(nullable = false, name = "created_at")
+    private OffsetDateTime createdAt;
+
+
+    @Column(name = "updated_at")
+    private OffsetDateTime updatedAt;
+
+
+    @Column(name = "deleted_at")
+    private OffsetDateTime deletedAt;
+
+
+    @Column(name = "state")
+    private String state;
+
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public OffsetDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(OffsetDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public OffsetDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(OffsetDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public OffsetDateTime getDeletedAt() {
+        return deletedAt;
+    }
+
+    public void setDeletedAt(OffsetDateTime deletedAt) {
+        this.deletedAt = deletedAt;
+    }
+
+    @PrePersist
+    protected void onCreate() {
+        createdAt = OffsetDateTime.now(ZoneOffset.UTC);
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        updatedAt = OffsetDateTime.now(ZoneOffset.UTC);
+    }
+
+    public void markAsDeleted() {
+        deletedAt = OffsetDateTime.now(ZoneOffset.UTC);
+    }
+
+    public boolean isDeleted() {
+        return deletedAt != null;
+    }
+    
+    @Override
+    public String toString() {
+        return "BaseEntity [id=" + id + ", createdAt=" + createdAt + ", updatedAt="
+                + updatedAt + ", deletedAt=" + deletedAt + "]";
+    }
+
+}
