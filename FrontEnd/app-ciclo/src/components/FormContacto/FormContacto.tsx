d9bb2a9e6644c3521a6c4c6218f13c999ef5668b
Roman Maluf <romanmaluf@vaypol.com.ar>
2023-06-20 19:29:54 -0300

    WIP: conectar formContacto con API de Backend. TODO: manejar errores, y manejar exito

--- FrontEnd/app-ciclo/src/components/FormContacto/FormContacto.tsx
+++ FrontEnd/app-ciclo/src/components/FormContacto/FormContacto.tsx
@@ -1,51 +1,79 @@
 import { useState } from "react";
-import { Form, Input, Label } from "semantic-ui-react";
+import { Form, Select } from "semantic-ui-react";
+import { useForm } from "react-hook-form";
 import styles from "./formcontacto.module.scss";
-const options = [{ key: "1", text: "Construcción de cero", value: "1" }];
+import api from "../../api";
+const options = [
+  { key: "1", text: "Construcción de cero", value: "CONSTRUCCION_DE_CERO" },
+  { key: "2", text: "Remodelación", value: "REMODELACION" },
+];
 
 const FormContacto = (props: any) => {
+  const { register, handleSubmit, setValue, reset } = useForm();
   const { stateChanger } = props;
   const [isOpen, setIsOpen] = useState(true);
-  const handleSubmit = (e: any) => {
-    e.preventDefault();
-    console.log(e);
-    return console.log("Submitting");
+  const onSubmit = async (data: any) => {
+    const response = await api.formProjects.create(data);
+    if (response.success) {
+      reset();
+    } else {
+      alert(response);
+    }
   };
 
   const handleClose = () => {
-    // TODO handle This to blank Formulario
+    reset();
     setIsOpen(false);
     stateChanger(false);
+  };
+
+  const handleSelectChange = (value: any) => {
+    setValue("projectType", value); // Set the value of the "projectType" field
   };
 
   return isOpen ? (
     <div className={styles.container}>
       <div className={styles.cross} onClick={() => handleClose()}></div>
-      <Form onSubmit={handleSubmit}>
-        <Form.Input
-          fluid
-          required
-          label="NOMBRE Y APELLIDO"
-          placeholder="Ciclo Soluciones Constructivas"
-        />
-        <Form.Input
-          fluid
-          required
-          label="TELÉFONO"
-          placeholder="+549 261 276 5262"
-        />
-        <Form.Input
-          fluid
-          required
-          label="EMAIL"
-          placeholder="info@ciclosoluciones.com"
-        />
+      <Form onSubmit={handleSubmit(onSubmit)}>
+        <Form.Field fluid required>
+          <label htmlFor="name">NOMBRE Y APELLIDO</label>
+          <input
+            type="text"
+            id="name"
+            {...register("name")}
+            placeholder="Ciclo Soluciones Constructivas"
+            required
+          />
+        </Form.Field>
+        <Form.Field fluid required>
+          <label htmlFor="name">TELÉFONO</label>
+          <input
+            type="text"
+            id="phoneNumber"
+            {...register("phoneNumber")}
+            placeholder="+549 261 276 5262"
+            required
+          />
+        </Form.Field>
+        <Form.Field fluid required>
+          <label htmlFor="name">EMAIL</label>
+          <input
+            type="text"
+            id="email"
+            {...register("email")}
+            placeholder="info@ciclosoluciones.com"
+            required
+          />
+        </Form.Field>
+
         <Form.Select
           fluid
           required
+          control={Select}
           label="INDICANOS EL TIPO DE PROYECTO"
           options={options}
           placeholder="Tipo de Proyecto"
+          onChange={(e: any, { value }: any) => handleSelectChange(value)} // Pass the selected value to the handler function
         />
         <Form.Button content="Iniciar Proyecto" circular color="pink" />
       </Form>
