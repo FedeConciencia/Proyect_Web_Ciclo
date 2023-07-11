5966a72869070b0c6f00296bbbbfb02c144c4da8
Roman Maluf <romanmaluf@vaypol.com.ar>
2023-07-10 22:45:09 -0300

    Agregamos crypto.js y agregamos encriptado para setear la cookie del login, para el dashboard del admin

--- FrontEnd/app-ciclo/src/middleware.ts
+++ FrontEnd/app-ciclo/src/middleware.ts
@@ -1,21 +1,40 @@
 import { NextRequest, NextResponse } from "next/server";
-const isPasswordEnabled = !!process.env.PASSWORD_PROTECT
-export async function middleware(req: NextRequest){
-    const isLoggedIn = req.cookies.has('login');
-    const isPathPasswordProtect = req.nextUrl.pathname.startsWith("/password-protect")
-    if(isPasswordEnabled && !isLoggedIn && !isPathPasswordProtect){
-        return NextResponse.redirect(new URL("/password-protect", req.url))
-    }
-    return NextResponse.next()
-}
-export const config = {
-    matcher: [
-      /*
-       * Match all request paths except for the ones starting with:
-       * - api (API routes)
-       * - _next/static (static files)
-       * - favicon.ico (favicon file)
-       */
-      '/(admin.*)',
-    ],
+const isPasswordEnabled = !!process.env.PASSWORD_PROTECT;
+const password = process.env.PASSWORD_PROTECT
+const encryptKey = process.env.ENCRYPT_KEY || '';
+import CryptoJS from "crypto-js";
+
+
+export async function middleware(req: NextRequest) {
+  const isLoggedIn = req.cookies.has("login");
+  const loginValue = req.cookies.get("login")?.value || ''
+
+  // Desncriptamos el valor de la cookie
+  const decrypted = CryptoJS.AES.decrypt(loginValue, encryptKey).toString(
+    CryptoJS.enc.Utf8
+  );
+
+  // Revisamos Si machea con la contraseña de las .env
+  const matchLoggin = isLoggedIn
+    ? decrypted == password
+    : false;
+
+  const isPathPasswordProtect = req.nextUrl.pathname.startsWith("/password-protect");
+  // Revisamos si esta todo correcto, para logearse o puede seguir navegando
+  if (isPasswordEnabled && !matchLoggin && !isPathPasswordProtect) {
+    return NextResponse.redirect(new URL("/password-protect", req.url));
   }
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - _next/static (static files)
+     * - favicon.ico (favicon file)
+     */
+    "/(admin.*)",
+  ],
+};
--- FrontEnd/app-ciclo/src/pages/api/password-protect/index.tsx
+++ FrontEnd/app-ciclo/src/pages/api/password-protect/index.tsx
@@ -1,6 +1,8 @@
 // api/password-protect.ts
 import { NextApiRequest, NextApiResponse } from "next";
 import { serialize } from "cookie";
+import CryptoJS from "crypto-js";
+
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
@@ -8,9 +10,20 @@ export default async function handler(
   if (req.method !== "POST") {
     res.status(405).send("Method Not Allowed");
   }
+
   const password = req.body.password;
+
   if (process.env.PASSWORD_PROTECT === password) {
-    const cookie = serialize("login", "true", {
+    // Si las contraseñas coinciden
+    const encryptKey = process.env.ENCRYPT_KEY || "";
+    const passowrdProtect = process.env.PASSWORD_PROTECT || "";
+    // Encriptamos la contraseña
+    const passwordEnctrypted = CryptoJS.AES.encrypt(
+      passowrdProtect,
+      encryptKey
+    );
+    // Y la guardamos en las cookies
+    const cookie = serialize("login", passwordEnctrypted.toString(), {
       path: "/",
       httpOnly: true,
     });
