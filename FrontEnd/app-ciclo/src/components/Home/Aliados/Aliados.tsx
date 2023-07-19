bbb12855d00b8d07038ef81387871feedb5cd1d4
Roman Maluf <romanmaluf@vaypol.com.ar>
2023-07-19 18:31:26 -0300

    Links en Slider de Aliados

--- FrontEnd/app-ciclo/src/components/Home/Aliados/Aliados.tsx
+++ FrontEnd/app-ciclo/src/components/Home/Aliados/Aliados.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 import styles from "./aliados.module.scss";
 
 const Aliados = () => {
@@ -8,54 +9,66 @@ const Aliados = () => {
         <div className={styles.second_column_images}>
           <div>
             <div className={styles.card_brand}>
-              <Image
-                src="/ciclos/ltn_brand_dark.png"
-                alt="Grupo LTN"
-                width={120}
-                height={50}
-              />
+              <Link href={"https://grupoltn.com/"}>
+                <Image
+                  src="/ciclos/ltn_brand_dark.png"
+                  alt="Grupo LTN"
+                  width={120}
+                  height={50}
+                />
+              </Link>
             </div>
             <div className={styles.card_brand}>
-              <Image
-                src="/ciclos/itu_brand_dark.png"
-                alt="Instituto Tecnologico Universitario"
-                width={120}
-                height={50}
-              />
+              <Link href={"https://itu.uncuyo.edu.ar/"}>
+                <Image
+                  src="/ciclos/itu_brand_dark.png"
+                  alt="Instituto Tecnologico Universitario"
+                  width={120}
+                  height={50}
+                />
+              </Link>
             </div>
             <div className={styles.card_brand}>
-              <Image
-                src="/ciclos/newpanel_brand_dark.png"
-                alt="Sistema de construcción sismotermica"
-                width={120}
-                height={50}
-              />
+              <Link href={"https://newpanel.com.ar/"}>
+                <Image
+                  src="/ciclos/newpanel_brand_dark.png"
+                  alt="Sistema de construcción sismotermica"
+                  width={120}
+                  height={50}
+                />
+              </Link>
             </div>
           </div>
           <div>
             <div className={styles.card_brand}>
-              <Image
-                src="/ciclos/extrabrut_brand_dark.png"
-                alt="Extra Brut Construir Mejor"
-                width={120}
-                height={50}
-              />
+              <Link href={"https://www.extrabrut.com.ar/"}>
+                <Image
+                  src="/ciclos/extrabrut_brand_dark.png"
+                  alt="Extra Brut Construir Mejor"
+                  width={120}
+                  height={50}
+                />
+              </Link>
             </div>
             <div className={styles.card_brand}>
-              <Image
-                src="/ciclos/cassaforma_brand_dark.png"
-                alt="Sistema Constructivo Integral"
-                width={120}
-                height={50}
-              />
+              <Link href={"http://cassaforma.com/"}>
+                <Image
+                  src="/ciclos/cassaforma_brand_dark.png"
+                  alt="Sistema Constructivo Integral"
+                  width={120}
+                  height={50}
+                />
+              </Link>
             </div>
             <div className={styles.card_brand}>
-              <Image
-                src="/ciclos/ctm_brand_dark.png"
-                alt="Colegios Tecnicos Mendoza"
-                width={120}
-                height={50}
-              />
+              <Link href={"http://www.colegiotecnicos.org.ar/"}>
+                <Image
+                  src="/ciclos/ctm_brand_dark.png"
+                  alt="Colegios Tecnicos Mendoza"
+                  width={120}
+                  height={50}
+                />
+              </Link>
             </div>
           </div>
         </div>
